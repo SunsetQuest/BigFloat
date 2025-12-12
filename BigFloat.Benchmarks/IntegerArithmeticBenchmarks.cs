a5066c7b24047a7d2cd20e4bb0ecaf728453f80f
Ryan Scott White <sunsetquest@hotmail.com>
2025-12-12 15:53:50 -0800

    Optimize int operator fast paths and add coverage

---
+++ BigFloat.Benchmarks/IntegerArithmeticBenchmarks.cs
@@ -0,0 +1,42 @@
+using BenchmarkDotNet.Attributes;
+using BigFloatLibrary;
+
+namespace BigFloat.Benchmarks;
+
+[MemoryDiagnoser]
+[BenchmarkCategory("Core")]
+public class IntegerArithmeticBenchmarks
+{
+    [Params(-4, -3, -2, -1, 1, 2, 3, 4, 7, 16, 123456789)]
+    public int Factor { get; set; }
+
+    [Params(32, 256, 1024)]
+    public int OperandBits { get; set; }
+
+    private BigFloat _value = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var rand = new Random(OperandBits * 48611 + Factor);
+        int mantissaBits = OperandBits + BigFloat.GuardBits;
+        _value = BigFloat.RandomWithMantissaBits(
+            mantissaBits,
+            -OperandBits,
+            OperandBits,
+            logarithmic: true,
+            rand: rand);
+
+        if (_value.IsZero)
+        {
+            _value = new BigFloat(1, binaryScaler: OperandBits / 4);
+        }
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("CI")]
+    public BigFloat MultiplyByInt() => _value * Factor;
+
+    [Benchmark]
+    public BigFloat DivideByInt() => _value / Factor;
+}
