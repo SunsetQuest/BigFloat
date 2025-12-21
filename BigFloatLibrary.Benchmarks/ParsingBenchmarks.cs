bd0a4a817d498ffc73dec518387128d648ceb003
Ryan Scott White (SunsetQuest) <sunsetquest@hotmail.com>
2025-12-20 19:54:48 -0800

    Refactor benchmarks into separate source files
    Move FormattingBenchmarks and ParsingBenchmarks out of Program.cs into their own files (FormattingBenchmarks.cs and ParsingBenchmarks.cs) under the BigFloatLibrary.Benchmarks namespace. This improves code organization and separation of concerns without changing benchmark functionality.

---
+++ BigFloatLibrary.Benchmarks/ParsingBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigFloatLibrary.Benchmarks;
+
+
+[MemoryDiagnoser]
+internal class ParsingBenchmarks
+{
+    private readonly string _input = "-12345678901234567890.12345678901234567890";
+
+    [Benchmark]
+    public BigFloat ParseWithString() => BigFloat.Parse(_input);
+
+    [Benchmark]
+    public BigFloat ParseWithSpan()
+    {
+        BigFloat.TryParse(_input.AsSpan(), out BigFloat value);
+        return value;
+    }
+}
+
--- BigFloatLibrary.Benchmarks/Program.cs
+++ BigFloatLibrary.Benchmarks/Program.cs
@@ -26,35 +26,4 @@ internal static class Program
     }
 
 
-    [MemoryDiagnoser]
-    internal class FormattingBenchmarks
-    {
-        private readonly BigFloat _largeValue = BigFloat.Parse("-12345678901234567890.12345678901234567890", guardBitsIncluded: 16);
-        private readonly char[] _buffer = new char[512];
-
-        [Benchmark]
-        public string FormatWithToString() => _largeValue.ToString();
-
-        [Benchmark]
-        public bool FormatWithTryFormat()
-        {
-            return _largeValue.TryFormat(_buffer, out _, [], provider: null);
-        }
-    }
-
-    [MemoryDiagnoser]
-    internal class ParsingBenchmarks
-    {
-        private readonly string _input = "-12345678901234567890.12345678901234567890";
-
-        [Benchmark]
-        public BigFloat ParseWithString() => BigFloat.Parse(_input);
-
-        [Benchmark]
-        public BigFloat ParseWithSpan()
-        {
-            BigFloat.TryParse(_input.AsSpan(), out BigFloat value);
-            return value;
-        }
-    }
 }
